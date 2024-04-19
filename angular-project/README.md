fc1bed6259f2d9348863fae8798155f6133f3bed
Lokesh Mittal <lokesh.adonis@gmail.com>
2024-04-19 19:42:57 -0400

    empty project first checkin

--- angular-project/README.md
+++ angular-project/README.md
@@ -1,7 +1,3 @@
-# Learning material
-
-https://www.youtube.com/playlist?list=PL1BztTYDF-QNlGo5-g65Xj1mINHYk_FM9
-
 # AngularProject
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.2.
--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -1,26 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';       // 3rd party lib for 2-way binding : ngModel
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { MenuComponent } from './menu/menu.component';
-import { RndComponent } from './rnd/rnd.component';
-import { ContainerComponent } from './container/container.component';
-import { StudentListComponent } from './container/student-list/student-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    MenuComponent,
-    RndComponent,
-    ContainerComponent,
-    StudentListComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
