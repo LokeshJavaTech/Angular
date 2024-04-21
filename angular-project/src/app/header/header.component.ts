0ee869b1a717e04246fff79f2eed3deef315349a
Lokesh Mittal <lokesh.adonis@gmail.com>
2024-04-21 11:21:03 -0400

    8_9-Replacing template and styles with templateUrl and styleUrls

--- angular-project/src/app/header/header.component.ts
+++ angular-project/src/app/header/header.component.ts
@@ -2,8 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-header',
-    template: '<h3>eKart</h3>',
-    styles: ['h3 { color: blue;}']
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent{
 
